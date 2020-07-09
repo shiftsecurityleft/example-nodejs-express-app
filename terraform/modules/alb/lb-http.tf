0cfd97256e0234ba8611faf8cdb0dc494ce25e32
Seongyong Kim <seongyongkim@yahoo.com>
2020-07-08 17:33:03 -0700

    checkout in 1st commit

--- terraform/modules/alb/lb-http.tf
+++ terraform/modules/alb/lb-http.tf
@@ -1,3 +1,51 @@
+variable VPC_NAME {}
+
+variable SOURCE_CIDRS {
+  type = list
+}
+
+variable LB_NAME {}
+
+variable LB_DNS_NAME {
+  default = "wildcard"
+}
+
+variable "TAGS" {
+  type = map(string)
+}
+
+data "aws_iam_account_alias" "current" {}
+
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = [var.VPC_NAME]
+  }
+}
+
+data "aws_subnet_ids" "selected" {
+  vpc_id = data.aws_vpc.main.id
+
+  tags = {
+    Tier = "public"
+  }
+}
+
+# lb Security group
+# This is the group you need to edit if you want to restrict access to your application
+resource "aws_security_group" "lb" {
+  name        = var.LB_NAME
+  description = "controls access to the lb"
+  vpc_id      = data.aws_vpc.main.id
+
+  tags = merge(
+		var.TAGS,
+		{
+			Name = var.LB_NAME
+    }
+	)
+}
+
 resource "aws_security_group_rule" "allow_http" {
   type      = "ingress"
   from_port = 80
@@ -5,13 +53,77 @@ resource "aws_security_group_rule" "allow_http" {
   protocol  = "tcp"
 
   # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-  cidr_blocks = "${var.SOURCE_CIDRS}"
+  cidr_blocks = var.SOURCE_CIDRS
 
-  security_group_id = "${aws_security_group.lb.id}"
+  security_group_id = aws_security_group.lb.id
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = "${aws_lb.this.arn}"
+resource "aws_security_group_rule" "nonrestricted_egress" {
+  type      = "egress"
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+
+  # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.lb.id
+}
+
+data "aws_elb_service_account" "main" {}
+
+data "aws_iam_policy_document" "s3_lb_write" {
+  policy_id = "s3_lb_write"
+
+  statement = {
+    actions   = ["s3:PutObject"]
+    resources = ["${module.s3_logs.arn}/${var.LB_NAME}-lb/*"]
+
+    principals = {
+      identifiers = [data.aws_elb_service_account.main.arn]
+      type        = "AWS"
+    }
+  }
+}
+
+module "s3_logs" {
+  source             = "../modules/s3"
+
+  S3_BUCKET          = "${var.LB_NAME}-logs-${data.aws_iam_account_alias.current.account_alias}"
+  VERSIONING_ENABLED = false
+
+  TAGS = var.TAGS
+}
+
+resource "aws_s3_bucket_policy" "logs" {
+  bucket = module.s3_logs.id
+  policy = data.aws_iam_policy_document.s3_lb_write.json
+}
+
+resource "aws_lb" "this" {
+  name               = var.LB_NAME
+  load_balancer_type = "application"
+  internal           = false
+
+  subnets         = [data.aws_subnet_ids.selected.ids]
+  security_groups = [aws_security_group.lb.id]
+
+  access_logs {
+    bucket  = module.s3_logs.id
+    prefix  = "${var.LB_NAME}-lb"
+    enabled = true
+  }
+
+  tags = merge(
+		var.TAGS,
+		{
+			Name = var.LB_NAME
+    }
+	)
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
   port              = "80"
   protocol          = "HTTP"
 
@@ -26,21 +138,6 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-resource "aws_lb_listener_rule" "redirect_http_to_https" {
-  listener_arn = "${aws_lb_listener.http.arn}"
-
-  action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-
-  condition {
-    field  = "path-pattern"
-    values = ["*"]
-  }
+output "lb_dns_name" {
+  value = aws_lb.this.dns_name
 }
--- terraform/setup/setup.tf
+++ terraform/setup/setup.tf
@@ -28,14 +28,7 @@ module "lb-ext" {
   source = "../modules/alb"
 
   LB_NAME  = "${var.FAMILY}-ext"
-  DOMAIN   = "aws.shiftsecurityleft.cloud"
   VPC_NAME = "${module.vpc.vpc_name}"
-
-  # Meaning it is external
-  INTERNAL = false
-
-  # Meaning not proxied by CloudFlare so DNS is purely controled by AWS Route53
-  PROXYED = false
 
   #SOURCE_CIDRS = ["173.245.48.0/20", "103.21.244.0/22", "103.22.200.0/22", "103.31.4.0/22", "141.101.64.0/18", "108.162.192.0/18", "190.93.240.0/20", "188.114.96.0/20", "197.234.240.0/22", "198.41.128.0/17", "162.158.0.0/15", "104.16.0.0/12", "172.64.0.0/13", "131.0.72.0/22", "12.171.137.115/32", "199.120.242.115/32"]
   SOURCE_CIDRS = ["0.0.0.0/0"]
@@ -43,6 +36,7 @@ module "lb-ext" {
   TAGS = "${local.common_tags}"
 }
 
+/*
 module "s3_scan_results" {
   source = "../modules/s3-secure"
 
@@ -54,3 +48,4 @@ module "s3_scan_results" {
 
   TAGS = "${local.common_tags}"
 }
+*/
